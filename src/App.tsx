856fc3be37d26df9e9c1b703bac9c7984aefb9d6
iida nozomi <nozomi@mbp.local>
2020-06-22 20:43:21 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+export default () => {
+  return(
+    <Fragment>
+      <h1>Pick and Morty</h1>
+      <p>Pick your favorite episode!!!</p>
+    </Fragment>
+  )
+}
