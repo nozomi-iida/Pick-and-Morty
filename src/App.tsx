be511e41dd847b880ada521cce6fa90c64f31b99
iida nozomi <nozomi@mbp.local>
2020-06-22 21:09:06 +0900

    Store.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
+import { Store } from './Store';
 
 export default ():JSX.Element => {
+  const store = useContext(Store);
   return(
     <Fragment>
+      {console.log(store)};
       <h1>Pick and Morty</h1>
       <p>Pick your favorite episode!!!</p>
     </Fragment>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { StoreProvidor } from './Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StoreProvidor>
+      <App />
+  </StoreProvidor>, 
+  document.getElementById('root')
+);
